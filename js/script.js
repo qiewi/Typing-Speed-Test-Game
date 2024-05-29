655865926ddae98c76abe9d5bd9c2d6c7ec96164
qiewi <rizqiandhika250804@gmail.com>
2024-05-30 00:05:49 +0700

    created input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-const typingText = document.querySelector('.typing-text');
+const typingText = document.querySelector('.typing-text p'),
+inpField = document.querySelector('.wrapper .input-field');
+
+let charIndex = 0;
 
 function randomParagraph() {
     // Random paragraph
     let randIndex = Math.floor(Math.random() * paragraphs.length);
-    console.log(paragraphs[randIndex]);
+    paragraphs[randIndex].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    });
+
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+    
+}
+
+function initTyping() {
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    console.log(typedChar)
 }
 
 randomParagraph();
+inpField.addEventListener("input", initTyping);
