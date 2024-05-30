404939dd299dd211bcdafa1466b56fdee3f4d1fb
qiewi <rizqiandhika250804@gmail.com>
2024-05-30 14:30:07 +0700

    added wpm and cpm

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const typingText = document.querySelector('.typing-text p'),
 inpField = document.querySelector('.wrapper .input-field'),
 timeTag = document.querySelector(".time span b"),
-mistakeTag = document.querySelector(".mistake span");
+mistakeTag = document.querySelector(".mistake span"),
+wpmTag = document.querySelector(".wpm span"),
+cpmTag = document.querySelector(".cpm span");
 
 let 
-timer, maxTime = 60, timeLeft = maxTime, 
+timer, maxTime = 10, timeLeft = maxTime, 
 charIndex = mistakes = isTyping = 0;
 
 function randomParagraph() {
@@ -24,31 +26,41 @@ function initTyping() {
     const characters = typingText.querySelectorAll("span");
     let typedChar = inpField.value.split("")[charIndex];
 
-    if (!isTyping) {    
-        timer = setInterval(initTimer, 1000);
-        isTyping = true;
-    }
-    
-    if (typedChar == null) {
-        charIndex--;
-        if (characters[charIndex].classList.contains("incorrect")) {
-            mistakes--;
+    if (charIndex < characters.length -1 && timeleft > 0) {
+        if (!isTyping) {    
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
         }
-        characters[charIndex].classList.remove("correct", "incorrect");
-    } else {
-        if(characters[charIndex].innerText === typedChar) {
-            characters[charIndex].classList.add("correct");
+        
+        if (typedChar == null) {
+            charIndex--;
+            if (characters[charIndex].classList.contains("incorrect")) {
+                mistakes--;
+            }
+            characters[charIndex].classList.remove("correct", "incorrect");
         } else {
-            mistakes++;
-            characters[charIndex].classList.add("incorrect");
+            if(characters[charIndex].innerText === typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
         }
-        charIndex++;
+    
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+    
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+    
+        mistakeTag.innerText = mistakes;
+        wpmTag.innerText = wpm;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        inpField.value = "";
+        clearInterval(timer);
     }
-
-    characters.forEach(span => span.classList.remove("active"));
-    characters[charIndex].classList.add("active");
-
-    mistakeTag.innerText = mistakes;
 }
 
 function initTimer() {
